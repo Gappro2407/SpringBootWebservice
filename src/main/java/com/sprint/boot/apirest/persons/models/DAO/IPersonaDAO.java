d9edb121b0f87ebce24f6f736efbcf510bbad113
unknown <gustavoalavarez6a@gmail.com>
2020-08-19 17:01:51 -0500

    refactor

--- src/main/java/com/sprint/boot/apirest/persons/models/DAO/IPersonaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.boot.apirest.persons.models.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sprint.boot.apirest.persons.entity.Persona;
-
-public interface IPersonaDAO extends CrudRepository<Persona, Long>{
-	
-}
